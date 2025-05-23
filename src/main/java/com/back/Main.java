764b6e727974da44031a00b5cdc926569a257792
EoN <jaegyun1011@naver.com>
2025-05-23 12:18:04 +0900

    level 4 수정

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         boolean exit = false;
-        WiseSaying[] db = new WiseSaying[MAX_NUM];
+        WiseSaying[] db = new WiseSaying[MAX_NUM+1];
         int num = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -15,9 +15,11 @@ public class Main {
         System.out.println("== 명언 앱 ==");
         while(!exit) {
             System.out.print("명령) ");
-            String cmd = scanner.nextLine().trim();
+            String input = scanner.nextLine().trim();
+            String cmd = input.split("\\?")[0];
+            String[] param = cmd.contains("?") ? cmd.split("\\?")[1].split("&") : new String[]{};
 
-            switch(cmd.substring(0, 2)) {
+            switch(cmd) {
                 case "등록" -> {
                     System.out.print("명언 : ");
                     String content = scanner.nextLine().trim();
@@ -27,13 +29,16 @@ public class Main {
                     System.out.println(num + "번 명언이 등록되었습니다.");
                 }
                 case "삭제" -> {
-                    System.out.println("삭제");
+                    System.out.print("삭제");
                 }
                 case "수정" -> {
                     System.out.println("수정");
                 }
                 case "목록" -> {
-                    System.out.println("목록");
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+                    for(int i=num; i>0; i--)
+                        System.out.printf("%d / %s / %s\n", db[i].id, db[i].author, db[i].content);
                 }
                 case "종료" -> exit = true;
                 default -> System.out.println("잘못된 명령어입니다.");
@@ -41,17 +46,5 @@ public class Main {
         }
 
         scanner.close();
-    }
-
-    public static class WiseSaying {
-        public int id;
-        public String content;
-        public String author;
-
-        WiseSaying(int id, String content, String author) {
-            this.id = id;
-            this.content = content;
-            this.author = author;
-        }
     }
 }
